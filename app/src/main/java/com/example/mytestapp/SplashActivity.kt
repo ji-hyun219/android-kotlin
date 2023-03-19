8b8d898f55df827a8e745a65bccd4826b173e50b
ji-hyun219 <ix219@hanmail.net>
2023-03-19 10:54:43 +0900

    fix(Twice): 사진 삽입 (windowNoTitleBar, scaleType)

--- app/src/main/java/com/example/mytestapp/SplashActivity.kt
+++ app/src/main/java/com/example/mytestapp/SplashActivity.kt
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, MainActivity2::class.java))
             finish()
         }, 3000)
     }
