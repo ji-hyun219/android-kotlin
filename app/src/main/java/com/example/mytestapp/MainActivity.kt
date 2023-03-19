019d2b8c82b06b450d261aadf70af8f2aecd9984
ji-hyun219 <ix219@hanmail.net>
2023-03-19 09:19:11 +0900

    fix(BTS): 화면 클릭 이벤트처리 (findViewId, Toast)

--- app/src/main/java/com/example/mytestapp/MainActivity.kt
+++ app/src/main/java/com/example/mytestapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mytestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val image1 = findViewById<ImageView>(R.id.btsImage1)
+        image1.setOnClickListener {
+            Toast.makeText(this, "1번 클릭 완료!", Toast.LENGTH_LONG).show()
+        }
     }
+
+
 }
