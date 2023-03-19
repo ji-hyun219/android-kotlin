0d1eb06b2d5810eb628c9163c6c006f35c6b2722
ji-hyun219 <ix219@hanmail.net>
2023-03-19 09:32:42 +0900

    fix(BTS): 화면 전환 (Intent)

--- app/src/main/java/com/example/mytestapp/MainActivity.kt
+++ app/src/main/java/com/example/mytestapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mytestapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -14,8 +15,61 @@ class MainActivity : AppCompatActivity() {
         val image1 = findViewById<ImageView>(R.id.btsImage1)
         image1.setOnClickListener {
             Toast.makeText(this, "1번 클릭 완료!", Toast.LENGTH_LONG).show()
+
+            // 화면 전환
+            val intent = Intent(this, Bts1Activity::class.java)
+            startActivity(intent)
+        }
+
+        val image2 = findViewById<ImageView>(R.id.btsImage2)
+        image2.setOnClickListener {
+            Toast.makeText(this, "2번 클릭 완료!", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, Bts2Activity::class.java)
+            startActivity(intent)
+        }
+
+        val image3 = findViewById<ImageView>(R.id.btsImage3)
+        image3.setOnClickListener {
+            Toast.makeText(this, "3번 클릭 완료!", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, Bts3Activity::class.java)
+            startActivity(intent)
+        }
+
+        val image4 = findViewById<ImageView>(R.id.btsImage4)
+        image4.setOnClickListener {
+            Toast.makeText(this, "4번 클릭 완료!", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, Bts4Activity::class.java)
+            startActivity(intent)
+        }
+
+        val image5 = findViewById<ImageView>(R.id.btsImage5)
+        image5.setOnClickListener {
+            Toast.makeText(this, "5번 클릭 완료!", Toast.LENGTH_LONG).show()
+
+            // 화면 전환
+            val intent = Intent(this, Bts5Activity::class.java)
+            startActivity(intent)
+        }
+
+        val image6 = findViewById<ImageView>(R.id.btsImage6)
+        image6.setOnClickListener {
+            Toast.makeText(this, "6번 클릭 완료!", Toast.LENGTH_LONG).show()
+
+            // 화면 전환
+            val intent = Intent(this, Bts6Activity::class.java)
+            startActivity(intent)
+        }
+
+        val image7 = findViewById<ImageView>(R.id.btsImage7)
+        image7.setOnClickListener {
+            Toast.makeText(this, "7번 클릭 완료!", Toast.LENGTH_LONG).show()
+
+            // 화면 전환
+            val intent = Intent(this, Bts7Activity::class.java)
+            startActivity(intent)
         }
     }
-
-
 }
